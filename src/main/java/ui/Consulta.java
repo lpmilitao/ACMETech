d37e050352ef53459115dff14d6027968199b4f2
lpmilitao <luizamilitaop@gmail.com>
2025-11-25 23:01:26 -0300

    adicionando base da tela de consultas

--- src/main/java/ui/Consulta.java
+++ src/main/java/ui/Consulta.java
@@ -5,17 +5,31 @@ import dados.CatalogoCompradores;
 import dados.CatalogoFornecedores;
 import dados.CatalogoTecnologias;
 import dados.CatalogoVendas;
-import ui.components.TelaBase;
+import entidades.Comprador;
+import ui.components.*;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Consulta extends TelaBase {
+public class Consulta<T> extends TelaBase {
     private final ACMETech APLICACAO;
     private final CatalogoFornecedores FORNECEDORES;
     private final CatalogoCompradores COMPRADORES;
     private final CatalogoTecnologias TECNOLOGIAS;
     private final CatalogoVendas VENDAS;
     private JPanel panelConsulta;
+    private JLabel titulo;
+    private JPanel panelBotoes;
+    private JButton botaoTecnologia;
+    private JButton botaoFornecedor;
+    private JButton botaoComprador;
+    private JButton botaoVenda;
+    private JButton botaoVoltar;
+    private JScrollPane panelResultado;
+    private JTextPane resultado;
+    private List<T> lista;
 
     public Consulta(ACMETech APLICACAO, CatalogoFornecedores FORNECEDORES, CatalogoCompradores COMPRADORES,
                     CatalogoTecnologias TECNOLOGIAS, CatalogoVendas VENDAS) {
@@ -24,10 +38,70 @@ public class Consulta extends TelaBase {
         this.COMPRADORES = COMPRADORES;
         this.TECNOLOGIAS = TECNOLOGIAS;
         this.VENDAS = VENDAS;
+        this.lista = new ArrayList<>();
+        ajustarScrollPane();
     }
 
     @Override
     public JPanel getPanel() {
         return panelConsulta;
+    }
+
+    private void createUIComponents() {
+
+        botaoTecnologia = new Botao("Tecnologia com maior valor");
+        botaoFornecedor = new Botao("Fornecedor com maior número de tecnologias");
+        botaoComprador = new Botao("Comprador com maior número de vendas");
+        botaoVenda = new Botao("Venda com maior valor");
+        botaoVoltar = new Botao("Voltar ao Menu");
+
+        botaoTecnologia.addActionListener(e -> consultarTecnologia());
+        botaoFornecedor.addActionListener(e -> consultarFornecedor());
+        botaoComprador.addActionListener(e -> consultarComprador());
+        botaoVenda.addActionListener(e -> consultarVenda());
+        botaoVoltar.addActionListener(e -> APLICACAO.mudarTela(Telas.MENU));
+
+        resultado = new AreaTexto();
+
+        titulo = new Texto(true);
+    }
+
+    private void consultarTecnologia() {
+        atualizarLista();
+    }
+
+    private void consultarFornecedor() {
+        atualizarLista();
+    }
+
+    private void consultarComprador() {
+        atualizarLista();
+    }
+
+    private void consultarVenda() {
+
+        atualizarLista();
+    }
+
+    private void atualizarLista() {
+        resultado.setText("");
+
+        StringBuilder listaFinal = new StringBuilder();
+
+        for (T objeto : lista) {
+            listaFinal.append(objeto + "\n");
+        }
+
+        resultado.setText(listaFinal.toString());
+    }
+
+    private void ajustarScrollPane() {
+        if (panelResultado != null) {
+            panelResultado.setOpaque(false);
+            panelResultado.getViewport().setOpaque(false);
+            panelResultado.setBorder(null);
+            panelResultado.getVerticalScrollBar().setPreferredSize(new Dimension(0, 0));
+            panelResultado.getVerticalScrollBar().setUnitIncrement(16);
+        }
     }
 }
